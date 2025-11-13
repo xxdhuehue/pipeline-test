b8b1bc0e7beb69e4ec65de93507337dddaf81f4b
noahxia <ambition0000@qq.com>
2025-11-13 16:23:07 +0800

    feat: change the title

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! really!!" />
+      <HelloWorld msg="You did it! really!! you can trust yourself!!!!" />
     </div>
   </header>
 
