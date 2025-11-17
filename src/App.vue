19f4e593a2d2e0f56745a47829085c634fca9058
NoahXia <128689531+xxdhuehue@users.noreply.github.com>
2025-11-17 10:29:28 +0800

    Update greeting message in HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hi Noah" />
+      <HelloWorld msg="Hi Noah, how are you?" />
     </div>
   </header>
 
